98754142aecf1a1220aba915f748723b889bd72d
El-Zieny <abdoelzieny72@gmail.com>
2023-09-11 08:20:32 +0300

    1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+		putchar(x);
+	for (x = 65; x < 91; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
