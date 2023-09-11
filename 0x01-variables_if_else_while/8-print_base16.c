f51e02ab1a191ce5170f63e308dea5bc5f8be00d
El-Zieny <abdoelzieny72@gmail.com>
2023-09-11 10:15:49 +0300

    1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+		putchar(x);
+	for (x = 97; x < 103; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
