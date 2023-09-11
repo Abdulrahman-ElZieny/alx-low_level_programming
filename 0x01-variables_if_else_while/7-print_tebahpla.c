57d73096d8d5fa484de3177e38d7cdc24ad3d7aa
El-Zieny <abdoelzieny72@gmail.com>
2023-09-11 10:13:57 +0300

    1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 122; x > 96; x--)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
