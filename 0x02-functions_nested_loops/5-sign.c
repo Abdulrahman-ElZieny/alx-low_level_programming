4708b6f16cc867db199aa68f1d99212a379189ee
El-Zieny <abdoelzieny72@gmail.com>
2023-09-12 14:53:25 +0300

    functions|nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_signe - a func to print the sign of a number (- or + or 0)
+ * print_sign - a func to print the sign of a number (- or + or 0)
  * @n: the number to check it's sign
  * Return: 1 if positive, -1 if negative, 0 if 0
  */
@@ -12,7 +12,7 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n == 0)
-	{       
+	{
 		_putchar('0');
 		return (0);
 	}
