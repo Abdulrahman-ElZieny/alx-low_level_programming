b4f8779ef72fcf42b04dd5982746cd69205156d2
El-Zieny <abdoelzieny72@gmail.com>
2023-09-13 01:54:26 +0300

    functions|nested_loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from number(n) to 98
+ * @n: start point
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("98\n");
+		return;
+	}
+	else if (n > 98)
+	{
+		for (;n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+		putchar('\n');
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+		putchar('\n');
+	}
+}
