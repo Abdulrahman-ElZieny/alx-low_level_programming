03762042baf5f685b513bb586e15346779b4accd
abdulrahman elzieny <abdoelzieny27@gmail.com>
2023-09-14 16:34:57 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n: the number of lines
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (x = 0; x <= n; n++)
+		_putchar('_');
+}
