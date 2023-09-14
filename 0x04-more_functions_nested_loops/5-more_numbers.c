7cacb7bccb2b0e750890ff9738cda15d794e9408
abdulrahman elzieny <abdoelzieny27@gmail.com>
2023-09-14 16:29:17 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ */
+void more_numbers(void)
+{
+	int x, i;
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar('1');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
