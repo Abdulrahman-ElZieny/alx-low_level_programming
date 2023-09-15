b2c2eccb04b0fadde8fc8d1ba0109a8f0b83662c
abdulrahman elzieny <abdoelzieny27@gmail.com>
2023-09-15 11:42:45 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x, t, f;
+
+	for (x = 1; x <= 100; x++)
+	{
+		t = x % 3;
+		f = x % 5;
+		if (x)
+			putchar(' ');
+		if (!t && !f)
+			printf("FizzBuzz");
+		else if (!t)
+			printf("Fizz");
+		else if (!f)
+			printf("Buzz");
+		else
+			printf("%d", x);
+	}
+	putchar('\n');
+
+	return (0);
+}
