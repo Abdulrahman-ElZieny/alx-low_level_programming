15b0d673a8cbc14830724610543583cd9ce8fd4d
abdulrahman elzieny <abdoelzieny27@gmail.com>
2023-09-15 10:27:26 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints '\' diagonally
+ * @n: the number of times the '\' should be printed
+ */
+void print_diagonal(int n)
+{
+	int x, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (x = 0; x < n; x++)
+	{
+		for (i = 0; i < x; i++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
