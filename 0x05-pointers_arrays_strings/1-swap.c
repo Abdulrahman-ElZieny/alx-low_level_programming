29ff8b832a734a69aab3fc1d740c6f6e81ea26d1
abdulrahman elzieny <abdoelzieny27@gmail.com>
2023-09-19 07:26:43 -0700

    c|pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swap the value of 2 pointers with each other
+ * @a: pointer 1
+ * @b: pointer 2
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
